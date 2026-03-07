3bd9702307af024634ac8e7c3eb20b46815d9a23
Matheus Pozett <maa_souza11@hotmail.com>
2026-03-07 20:32:33 -0300

    feat : cria controller para person

---
+++ src/main/java/com/betrybe/agrix/controller/PersonController.java
@@ -0,0 +1,46 @@
+package com.betrybe.agrix.controller;
+
+import com.betrybe.agrix.dto.PersonCreationDto;
+import com.betrybe.agrix.dto.PersonDto;
+import com.betrybe.agrix.model.entity.Person;
+import com.betrybe.agrix.service.PersonService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller for managing persons.
+ * Handles HTTP requests related to person operations.
+ */
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+  private final PersonService personService;
+
+  @Autowired
+  public PersonController(PersonService personService) {
+    this.personService = personService;
+  }
+
+  /**
+   * Creates a new person.
+   * This endpoint accepts a person creation request and returns the created person data.
+   *
+   * @param personCreationDto the DTO containing the data for creating a new person
+   * @return a ResponseEntity containing the created PersonDto with HTTP status 201 (Created)
+   */
+  @PostMapping
+  public ResponseEntity<PersonDto> createPerson(
+      @Valid @RequestBody PersonCreationDto personCreationDto
+  ) {
+    Person person = personService.create(personCreationDto.toEntity());
+    PersonDto personDto = PersonDto.fromEntity(person);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(personDto);
+  }
+}
