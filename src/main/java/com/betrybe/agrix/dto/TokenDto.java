465bc39770ba3bf1e872faee8a50983b8ed74c6b
Matheus Pozett <maa_souza11@hotmail.com>
2026-03-11 10:15:39 -0300

    feat: cria service para gerar token jwt

---
+++ src/main/java/com/betrybe/agrix/dto/TokenDto.java
@@ -0,0 +1,4 @@
+package com.betrybe.agrix.dto;
+
+public record TokenDto(String token) {
+}
---
+++ src/main/java/com/betrybe/agrix/service/TokenService.java
@@ -0,0 +1,27 @@
+package com.betrybe.agrix.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
+public class TokenService {
+  private final Algorithm algorithm;
+
+  public TokenService(@Value("${api.security.token.secret}") String secret) {
+    this.algorithm = Algorithm.HMAC256(secret);
+  }
+
+  public String generateToken(String username) {
+    return JWT.create().withSubject(username).withExpiresAt(generateExpiration()).sign(algorithm);
+  }
+
+  public Instant generateExpiration() {
+    return Instant.now().plus(2, ChronoUnit.HOURS);
+  }
+}
